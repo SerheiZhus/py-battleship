ea354a58baa47ea538dded6c4d020a15c0923e6f
sergey <38cerg12021979@jmal.com>
2024-03-28 18:21:06 +0200

    28_03_24

--- app/main.py
+++ app/main.py
@@ -1,34 +1,84 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(self,
+                 start: tuple,
+                 end: tuple,
+                 is_drowned: bool = False) -> None:
+        self.decks = [Deck(row, column) for row in range(start[0], end[0] + 1)
+                      for column in range(start[1], end[1] + 1)]
+        self.is_drowned = is_drowned
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def get_deck(self, row: int, column: int) -> Deck | None:
+        return next((deck for deck in self.decks if deck.row
+                     == row and deck.column == column), None)
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def fire(self, row: int, column: int) -> None:
+        deck = self.get_deck(row, column)
+        if deck:
+            deck.is_alive = False
+            if all(not deck.is_alive for deck in self.decks):
+                self.is_drowned = True
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(self, ships: list[tuple]) -> None:
+        self.field = [["~" for _ in range(10)] for _ in range(10)]
+        self.ships = [self._create_ship(start, end) for start, end in ships]
+        self._validate_field()
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+    def _create_ship(self, start: tuple, end: tuple) -> list[tuple]:
+        ship = [(row, column) for row in range(start[0],
+                end[0] + 1) for column in range(start[1], end[1] + 1)
+                ]
+        for row, column in ship:
+            self.field[row][column] = "□"
+        return ship
+
+    def fire(self, ceil: tuple) -> str:
+        row, column = ceil
+        if self.field[row][column] == "~":
+            return "Miss!"
+        elif self.field[row][column] == "□":
+            self.field[row][column] = "*"
+            if self._is_sunk(ceil):
+                for ship in self.ships:
+                    if ceil in ship:
+                        for row, column in ship:
+                            self.field[row][column] = "x"
+                return "Sunk!"
+            else:
+                return "Hit!"
+
+    def _is_sunk(self, ceil: tuple) -> bool:
+        for ship in self.ships:
+            if ceil in ship and all(self.field[row][column]
+                                    == "*" for row, column in ship):
+                return True
+        return False
+
+    def print_field(self) -> None:
+        for row in self.field:
+            print(" ".join(row))
+        print()
+
+    def _validate_field(self) -> None:
+        ship_counts = [0, 4, 3, 2, 1]
+        for ship in self.ships:
+            ship_len = len(ship)
+            assert ship_len in [1, 2, 3, 4], f"Invalid ship length: {ship_len}"
+            ship_counts[ship_len] -= 1
+            for row, column in ship:
+                for point_x in range(max(0, row - 1), min(10, row + 2)):
+                    for point_y in range(max(0, column - 1),
+                                         min(10, column + 2)):
+                        if ((point_x, point_y) not in ship
+                                and self.field[point_x][point_y] != "~"):
+                            raise ValueError("Ships are too close")
+        assert all(count == 0 for count in ship_counts), \
+            "Invalid number of ships"
